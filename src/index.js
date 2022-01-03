5a4641014b7ef89d21db6af5701aed9f0c145194
gavrilenkoartem038 <gavrilenkoartem038@gmail.com>
2022-01-03 22:33:27 +0300

    done

--- src/index.js
+++ src/index.js
@@ -1,3 +1,22 @@
-module.exports = function reverse (n) {
-  
+module.exports = function reverse (number) {
+    let strNumber = number.toString()
+  let result = ''
+  if (isPositive (number)) {
+    for (let i = strNumber.length - 1; i >= 0; i--) {
+      result += strNumber[i]
+    }
+  } else {
+    for (let i = strNumber.length - 1; i > 0; i--) {
+      result += strNumber[i]
+    }
+  }
+  result = + result
+  return result
 }
+
+function isPositive (number) {
+    if (number.toString()[0] === '-') {
+      return false
+    }
+    return true
+  }
